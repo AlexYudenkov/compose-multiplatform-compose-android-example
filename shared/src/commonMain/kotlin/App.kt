baa8a13c2a7aba2edd7fc6f86a4705837cfc9783
alex <alexydenkov@gmail.com>
2023-05-04 11:30:18 +0700

    init commit

---
+++ shared/src/commonMain/kotlin/App.kt
@@ -0,0 +1,40 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            repeat(10) {
+                SimpleItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleItem() {
+    Button(
+        modifier = Modifier.size(200.dp, 400.dp),
+        onClick = {}
+    ) {
+        Text("Hello, World!")
+    }
+}
